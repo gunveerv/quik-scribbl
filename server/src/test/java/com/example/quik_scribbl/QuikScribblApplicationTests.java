d3aa98291fd5409726897df1dfc2bd269540a553
Gunveer Vilkhu <gvilkhu@uwo.ca>
2024-08-28 18:13:07 -0400

    inital

---
+++ server/src/test/java/com/example/quik_scribbl/QuikScribblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quik_scribbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuikScribblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
